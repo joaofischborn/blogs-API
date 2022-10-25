746d844012da42f5e7f55ee11c5dc779ac958211
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-25 17:29:02 -0300

    Requisito 02 ok

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: DataTypes.INTEGER,
+      display_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    });
+    
+    return User;
+};
+  
+ 
