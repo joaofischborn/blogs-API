d6e0e1ad233b35a61189e9f94e153cc36ff43160
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-30 11:58:49 -0300

    Requisito 11 ok

---
+++ src/models/PostCategory.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        post_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+    },
+    {
+        tableName: 'post_categories',
+        timestamps: false,
+    });
+
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+        foreignKey: 'post_id',
+          as: 'post_id',
+          otherKey: 'category_id',
+          through: PostCategory,
+        });
+    
+        models.Category.belongsToMany(models.BlogPost, {
+        foreignKey: 'category_id',
+          as: 'category_id',
+          otherKey: 'post_id',
+          through: PostCategory,
+        });
+      }
+
+      return PostCategory;
+}
